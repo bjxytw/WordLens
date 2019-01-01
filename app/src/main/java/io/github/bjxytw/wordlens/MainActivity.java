d5bc8aa9e4ece3449214bf3da4a5c6d13fc9341f
Ard <bjxytw@gmail.com>
2019-01-02 04:36:19 +0900

    2

--- app/src/main/java/io/github/bjxytw/wordlens/MainActivity.java
+++ app/src/main/java/io/github/bjxytw/wordlens/MainActivity.java
@@ -33,37 +33,32 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preview = findViewById(R.id.firePreview);
-        if (preview == null) {
+        if (preview == null)
             Log.d(TAG, "Preview is null");
-        }
-        graphicOverlay = findViewById(R.id.fireFaceOverlay);
-        if (graphicOverlay == null) {
-            Log.d(TAG, "graphicOverlay is null");
-        }
 
-        if (allPermissionsGranted()) {
+        graphicOverlay = findViewById(R.id.fireFaceOverlay);
+        if (graphicOverlay == null)
+            Log.d(TAG, "graphicOverlay is null");
+
+        if (allPermissionsGranted())
             createCameraSource();
-        } else {
-            getRuntimePermissions();
-        }
+        else getRuntimePermissions();
     }
 
     private void createCameraSource() {
-        if (cameraSource == null) {
+        if (cameraSource == null)
             cameraSource = new CameraSource(this, graphicOverlay);
-        }
+
         cameraSource.setMachineLearningFrameProcessor(new TextRecognitionProcessor());
     }
 
     private void startCameraSource() {
         if (cameraSource != null) {
             try {
-                if (preview == null) {
+                if (preview == null)
                     Log.d(TAG, "resume: Preview is null");
-                }
-                if (graphicOverlay == null) {
+                if (graphicOverlay == null)
                     Log.d(TAG, "resume: graphOverlay is null");
-                }
                 preview.start(cameraSource, graphicOverlay);
             } catch (IOException e) {
                 Log.e(TAG, "Unable to start camera source.", e);
@@ -80,7 +75,6 @@ public final class MainActivity extends AppCompatActivity {
         startCameraSource();
     }
 
-    /** Stops the camera. */
     @Override
     protected void onPause() {
         super.onPause();
@@ -90,47 +84,35 @@ public final class MainActivity extends AppCompatActivity {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (cameraSource != null) {
+        if (cameraSource != null)
             cameraSource.release();
-        }
-    }
-
-    private String[] getRequiredPermissions() {
-        try {
-            PackageInfo info =
-                    this.getPackageManager()
-                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
-            String[] ps = info.requestedPermissions;
-            if (ps != null && ps.length > 0) {
-                return ps;
-            } else {
-                return new String[0];
-            }
-        } catch (Exception e) {
-            return new String[0];
-        }
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String permission : getRequiredPermissions()) {
-            if (!isPermissionGranted(this, permission)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private void getRuntimePermissions() {
         List<String> allNeededPermissions = new ArrayList<>();
         for (String permission : getRequiredPermissions()) {
-            if (!isPermissionGranted(this, permission)) {
+            Log.i(TAG, "requiredPermission: " + permission);
+
+            if (isPermissionNotGranted(this, permission))
                 allNeededPermissions.add(permission);
-            }
         }
 
-        if (!allNeededPermissions.isEmpty()) {
-            ActivityCompat.requestPermissions(
-                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        if (!allNeededPermissions.isEmpty())
+            ActivityCompat.requestPermissions(this,
+                    allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info = this.getPackageManager()
+                    .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+
+            if (ps != null && ps.length > 0) return ps;
+            else return new String[0];
+
+        } catch (Exception e) {
+            return new String[0];
         }
     }
 
@@ -138,19 +120,22 @@ public final class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(
             int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.i(TAG, "Permission granted!");
-        if (allPermissionsGranted()) {
+
+        if (allPermissionsGranted())
             createCameraSource();
-        }
+
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private static boolean isPermissionGranted(Context context, String permission) {
-        if (ContextCompat.checkSelfPermission(context, permission)
-                == PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission granted: " + permission);
-            return true;
-        }
-        Log.i(TAG, "Permission NOT granted: " + permission);
-        return false;
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions())
+            if (isPermissionNotGranted(this, permission))
+                return false;
+        return true;
+    }
+
+    private static boolean isPermissionNotGranted(Context context, String permission) {
+        return ContextCompat.checkSelfPermission(context, permission)
+                != PackageManager.PERMISSION_GRANTED;
     }
 }
